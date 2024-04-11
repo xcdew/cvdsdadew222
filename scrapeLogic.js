1c9c77a3140a224dcb050ba4c029e8ab6b03e7f0
xcdew <164291094+xcdew@users.noreply.github.com>
2024-04-11 13:01:58 +0000

    Update scrapeLogic.js

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -18,13 +18,43 @@ const scrapeLogic = async (res) => {
     const pages = await browser.pages();
     const page = pages[0]; // Get the first page, which is the current tab
 
-    let n = 0;
-    function read() {
-        console.log(`Puppeteer has been running for ${n}s`)
-        n = n + 1;
+    await page.goto('https://www.sportybet.com/gh/m/games/sportygames?game=turbo-games/aviator');
+
+    await page.waitForSelector('input[type="tel"]');
+    await page.type('input[type="tel"]', '572028292');
+    await page.type('input[type="password"]', 'Tagoe342@$');
+
+    await page.click('.login-btn');
+
+    function delay(time) {
+      return new Promise(function(resolve) {
+        setTimeout(resolve, time)
+      });
     }
 
-    setInterval(read, 1000);
+    await delay (10000)
+
+    const frame = page.frames().find(frame => frame.url().includes('aviator-next.spribegaming.com'));
+
+    await frame.evaluate(() => {
+      function main(){
+        var button = document.querySelector('.btn.btn-warning.btn-claim');
+        if (button) {
+            button.click();
+        }
+    }
+
+    setInterval(main, 0);
+    });
+
+    seconds = 0;
+
+    function timer() {
+      seconds++;
+      console.log(`Running for ${seconds} s`);
+    }
+
+    setInterval(timer, 1000);
 
     
     const logStatement = `Running...`;
